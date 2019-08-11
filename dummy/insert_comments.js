a34aec951c1a28931633768cbfd2ead06291f6bb
unknown <=>
2019-08-11 21:57:13 +0430

    to add multiple files into db

--- dummy/insert_comments.js
+++ dummy/insert_comments.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const Comment = require('../models/Comment')
 const fs = require('fs')
 
+const comments_part_1 = fs.readFileSync('./dummy/COMMENTS_DATA_PART_1.json');
+const comments_part_2 = f.readFileSync('./dummy/COMMENTS_DATA_PART_2');
 const comments = fs.readFileSync('./dummy/COMMENTS_DATA_PART_3.JSON');
 
 mongoose.connect('mongodb://localhost:27017/blog', async (err) => {
-    //await Comment.remove({});
+    await Comment.remove({});
     console.log(JSON.parse(comments).length)
     if (err)
         return console.log(err);
